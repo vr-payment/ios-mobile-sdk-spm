104666311804664d95129299fd223f2f854817ad
wallee-deployment-user <technical-systems@wallee.com>
2026-07-24 14:18:33 +0200

    2.0.0

--- Package.swift
+++ Package.swift
@@ -5,23 +5,17 @@ let package = Package(
     name: "VRPaymentSdk",
     platforms: [.iOS("12.4")],
     products: [
-        .library(name: "VRPaymentSdk",
-                 targets: ["ThreeDS_SDK","VRPaymentSdk","TwintSDK","PaymentResources"]),
+        .library(name: "ReactBrownfield", targets: ["ReactBrownfield"]),
+        .library(name: "ThreeDS_SDK", targets: ["ThreeDS_SDK"]),
+        .library(name: "TwintSDK", targets: ["TwintSDK"]),
+        .library(name: "VRPaymentSdk", targets: ["VRPaymentSdk"]),
+        .library(name: "hermes", targets: ["hermes"])
     ],
     targets: [
-		.binaryTarget(name: "ThreeDS_SDK", path: "ThreeDS_SDK.xcframework"),
-		.binaryTarget(name: "VRPaymentSdk", path: "VRPaymentSdk.xcframework"),
-		.binaryTarget(name: "TwintSDK", path: "TwintSDK.xcframework"),
-	.target(
-		name: "PaymentResources",
-		dependencies: [
-			.target(name: "VRPaymentSdk")
-		],
-		path: "Sources/PaymentResources",
-		sources: ["PaymentResources.swift"],
-		resources: [
-			.process("vrpaymentsdkbundle.jsbundle")
-		]
-	)
+        .binaryTarget(name: "ReactBrownfield", path: "./ReactBrownfield.xcframework"),
+        .binaryTarget(name: "ThreeDS_SDK", path: "./ThreeDS_SDK.xcframework"),
+        .binaryTarget(name: "TwintSDK", path: "./TwintSDK.xcframework"),
+        .binaryTarget(name: "VRPaymentSdk", path: "./VRPaymentSdk.xcframework"),
+        .binaryTarget(name: "hermes", path: "./hermes.xcframework")
     ]
 )
